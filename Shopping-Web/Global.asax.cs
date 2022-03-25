180dc5742bebdcb0003494f35b51da060621824f
HyDra <swjoechen@gmail.com>
2022-03-25 21:02:45 +0800

    init

---
+++ Shopping-Web/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Shopping_Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Add Routes.
+            RegisterCustomRoutes(RouteTable.Routes);
+        }
+
+        void RegisterCustomRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "index",
+                "*",
+                "~/Index.html"
+            );
+            RouteTable.Routes.MapHttpRoute(
+                name: "AgentApi",
+                routeTemplate: "api/{controller}/{action}"
+            );
+        }
+    }
+}
