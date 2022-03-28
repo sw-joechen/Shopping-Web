14572cbd767790d37a4b472134cbcaf56f8584c2
I552\Administrator <aa>
2022-03-28 11:47:22 +0800

    [remove] 沒用組件, 更新tailwind config, i18n

--- Shopping-Web/client/src/locales/tw.js
+++ Shopping-Web/client/src/locales/tw.js
@@ -5,8 +5,13 @@ export default {
     search: '搜尋',
     add: '新增',
     success: '操作成功',
+    account: '密碼',
     pwd: '密碼',
     accountPlaceholder: '請輸入帳號',
     pwdPlaceholder: '請輸入密碼',
+    personalInfo: '個人資訊',
+    shoppingCart: '購物車',
+    logout: '登出',
+    login: '登入',
   },
 };
--- Shopping-Web/client/tailwind.config.js
+++ Shopping-Web/client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green1: '#42b883',
+        green2: '#33a06f',
+        green3: '#2c3e50',
+        green4: '#33a06f',
+      },
+    },
   },
   plugins: [],
 };
