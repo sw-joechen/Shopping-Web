c4b09f8179a8e82bc454bd661da3a0f1cf8ab05b
I552\Administrator <aa>
2022-03-28 10:54:20 +0800

    [add] i18n

---
+++ Shopping-Web/client/src/locales/tw.js
@@ -0,0 +1,12 @@
+export default {
+  common: {
+    edit: '編輯',
+    unlock: '解鎖',
+    search: '搜尋',
+    add: '新增',
+    success: '操作成功',
+    pwd: '密碼',
+    accountPlaceholder: '請輸入帳號',
+    pwdPlaceholder: '請輸入密碼',
+  },
+};
--- Shopping-Web/client/src/main.js
+++ Shopping-Web/client/src/main.js
@@ -3,11 +3,24 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './index.css';
+import VueI18n from 'vue-i18n';
+import tw from './locales/tw';
 
+Vue.use(VueI18n);
 Vue.config.productionTip = false;
+
+const messages = {
+  tw,
+};
+
+const i18n = new VueI18n({
+  locale: 'tw',
+  messages,
+});
 
 new Vue({
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
