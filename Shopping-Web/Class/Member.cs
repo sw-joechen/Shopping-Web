033252a3a1ce5ed4e999ee7aae56ced11558e21e
I552\Administrator <aa>
2022-03-31 15:01:32 +0800

    [add] updateMemberPwd API, getMemberPersonalInfo API

---
+++ Shopping-Web/Class/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping_Web.Class
+{
+    public class Member
+    {
+        public int id { get; set; }
+        public string account { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public int gender { get; set; }
+        public string email { get; set; }
+        public bool enabled { get; set; }
+        public string createdDate { get; set; }
+        public string updatedDate { get; set; }
+        public double balance { get; set; }
+        public string pwd { get; set; }
+    }
+}
--- Shopping-Web/Controllers/MemberController.cs
+++ Shopping-Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using Shopping_Web.Class;
+﻿using Newtonsoft.Json;
+using Shopping_Web.Class;
 using Shopping_Web.Models;
 using Shopping_Web.Validators;
 using System;
@@ -230,6 +231,114 @@ namespace Shopping_Web.Controllers
         }
 
         /// <summary>
+        /// 更新帳號
+        /// </summary>
+        [HttpPost]
+        [Route("api/{controller}/updateMemberPwd")]
+        public string updateMemberPwd()
+        {
+            var dict = new Dictionary<string, object>();
+            Result result = new Result(100, "缺少參數");
+            var httpRequest = HttpContext.Current.Request;
+
+            if (httpRequest.Params["account"] == null || httpRequest.Params["oldPwd"] == null || httpRequest.Params["newPwd"] == null)
+            {
+                result.Set(100, "缺少參數");
+                return result.Stringify();
+            }
+
+            try
+            {
+                string account = httpRequest.Params["account"];
+                string oldPwd = httpRequest.Params["oldPwd"];
+                string newPwd = httpRequest.Params["newPwd"];
+
+                // Hash new password
+                string hashedNewPwd = SecurePasswordHasher.Hash(newPwd);
+
+                Debug.WriteLine($"account=> {account}");
+                Debug.WriteLine($"oldPwd=> {oldPwd}");
+                Debug.WriteLine($"newPwd=> {newPwd}");
+
+                PwdValidator pwdValidator = new PwdValidator();
+
+                // 進庫撈使用者
+                using (SqlConnection conn = new SqlConnection(connectString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_getMemberByAccount", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@account", account);
+                        SqlDataReader r = cmd.ExecuteReader();
+                        if (r.Read())
+                        {
+                            dict["id"] = r["f_id"];
+                            dict["account"] = r["f_account"];
+                            dict["pwd"] = r["f_pwd"];
+                            dict["address"] = r["f_address"];
+                            dict["phone"] = r["f_phone"];
+                            dict["gender"] = r["f_gender"];
+                            dict["email"] = r["f_email"];
+                            dict["enabled"] = r["f_enabled"];
+                            dict["balance"] = r["f_balance"];
+                            dict["createdDate"] = r["f_createdDate"];
+                            dict["updatedDate"] = r["f_updatedDate"];
+                        }
+                        else
+                        {
+                            result.Set(105, "帳號錯誤");
+                            return result.Stringify();
+                        }
+                        // 關閉連線
+                        conn.Close();
+                    }
+                }
+                Debug.WriteLine($"dict=> {JsonConvert.SerializeObject(dict)}");
+
+                // check status
+                if (Convert.ToInt16(dict["enabled"]) == 0)
+                {
+                    result.Set(108, "該帳號已被禁用");
+                    return result.Stringify();
+                }
+
+                // check pwd
+                bool verify = SecurePasswordHasher.Verify(oldPwd, dict["pwd"].ToString());
+                if (verify)
+                {
+                    // 寫進庫
+                    using (SqlConnection conn = new SqlConnection(connectString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("pro_sw_editMemberPwd", conn))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@account", account);
+                            cmd.Parameters.AddWithValue("@newPwd", hashedNewPwd);
+                            SqlDataReader r = cmd.ExecuteReader();
+                            if (r.Read())
+                            {
+                                result.Set(200, "success");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // 密碼錯誤
+                    result.Set(104, "密碼錯誤");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"ex: {ex}");
+                result.Set(101, "網路錯誤");
+            }
+            return result.Stringify();
+        }
+
+        /// <summary>
         /// 登入帳號
         /// </summary>
         [HttpPost]
@@ -316,6 +425,71 @@ namespace Shopping_Web.Controllers
                 result.Set(101, "網路錯誤");
             }
 
+            return result.Stringify();
+        }
+
+        /// <summary>
+        /// 取得會員個人資訊
+        /// </summary>
+        [HttpPost]
+        [Route("api/{controller}/getMemberPersonalInfo")]
+        public string GetMemberPersonalInfo()
+        {
+            var httpRequest = HttpContext.Current.Request;
+            Result result = new Result(100, "缺少參數");
+            List<Member> memberList = new List<Member> { };
+
+            if (httpRequest.Params["account"] == null )
+            {
+                result.Set(100, "缺少參數");
+                return result.Stringify();
+            }
+
+            try
+            {
+                string account = httpRequest.Params["account"];
+
+                Debug.WriteLine($"account=> {account}");
+
+                using (SqlConnection conn = new SqlConnection(connectString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_getMemberByAccount", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@account", account);
+                        SqlDataReader r = cmd.ExecuteReader();
+
+                        if (r.HasRows)
+                        {
+                            while (r.Read())
+                            {
+                                memberList.Add(new Member
+                                {
+                                    id = Convert.ToInt16(r["f_id"]),
+                                    account = r["f_account"].ToString(),
+                                    address = r["f_address"].ToString(),
+                                    phone = r["f_phone"].ToString(),
+                                    gender = Convert.ToInt16(r["f_gender"]),
+                                    email = r["f_email"].ToString(),
+                                    enabled = Convert.ToBoolean(Convert.ToInt16(r["f_enabled"])),
+                                    createdDate = r["f_createdDate"].ToString(),
+                                    updatedDate = r["f_updatedDate"].ToString(),
+                                    balance = Convert.ToDouble(r["f_balance"]),
+                                    pwd = r["f_pwd"].ToString()
+                                });
+                            }
+
+                        }
+                        result.Set(200, "success", memberList);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"ex: {ex}");
+                result.Set(101, "網路錯誤");
+            }
             return result.Stringify();
         }
     }
