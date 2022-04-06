0ba423ab9016d754e2f11ce3b1dfe7cc60be30a2
I552\Administrator <aa>
2022-04-06 10:20:24 +0800

    [fixed] 商品卡片hover效果

--- Shopping-Web/client/src/components/Card.vue
+++ Shopping-Web/client/src/components/Card.vue
@@ -1,51 +1,55 @@
 <template>
-  <div class="card rounded overflow-hidden shadow-lg flex flex-col">
-    <div class="imgContainer relative w-full pt-[100%] bg-white">
-      <!--  TODO: svg可以放在background -->
-      <img
-        src="@/assets/spin_animated.svg"
-        class="w-20 h-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      />
-      <img
-        :class="`img${product.id}`"
-        class="img w-full h-full absolute top-0 left-0 object-contain align-bottom"
-        :data-temp="redirectImg(product.picture)"
-      />
-    </div>
-    <div class="content px-5 py-2 flex-grow flex flex-col">
+  <div class="card flex cursor-pointer">
+    <div class="wrapper rounded shadow-lg flex flex-col overflow-hidden">
       <div
-        :title="product.name"
-        class="name txtEllipsis font-bold text-2xl mb-2"
+        class="imgContainer relative w-full pt-[100%] bg-white overflow-hidden"
       >
-        {{ product.name }}
+        <!--  TODO: svg可以放在background -->
+        <img
+          src="@/assets/spin_animated.svg"
+          class="w-20 h-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
+        <img
+          :class="`img${product.id}`"
+          class="img w-full h-full absolute top-0 left-0 object-contain align-bottom hover:scale-125 transition duration-300"
+          :data-temp="redirectImg(product.picture)"
+        />
       </div>
-      <div
-        :title="product.description"
-        class="desc txtEllipsis text-gray-700 text-base flex-grow"
-      >
-        {{ product.description }}
-      </div>
-      <div class="amount flex justify-end mt-1">
-        <div>剩餘數量 {{ product.amount }}</div>
-      </div>
-      <div class="bottom flex">
-        <div class="price leading-10 text-xl text-red2 mr-2">
-          <span class="text-base">$</span>{{ product.price }}
+      <div class="content px-5 py-2 flex-grow flex flex-col">
+        <div
+          :title="product.name"
+          class="name txtEllipsis font-bold text-2xl mb-2"
+        >
+          {{ product.name }}
         </div>
-        <div class="btnsWrapper flex justify-between flex-grow">
-          <BtnPrimary
-            label="結帳"
-            @submit="CheckoutHandler"
-            theme="red"
-            px="px-3"
-          />
-          <BtnPrimary
-            theme="green"
-            class="whitespace-nowrap"
-            label="加入購物車"
-            @submit="AddToCartHandler"
-            px="px-2"
-          />
+        <div
+          :title="product.description"
+          class="desc txtEllipsis text-gray-700 text-base flex-grow"
+        >
+          {{ product.description }}
+        </div>
+        <div class="amount flex justify-end mt-1">
+          <div>剩餘數量 {{ product.amount }}</div>
+        </div>
+        <div class="bottom flex">
+          <div class="price leading-10 text-xl text-red2 mr-2">
+            <span class="text-base">$</span>{{ product.price }}
+          </div>
+          <div class="btnsWrapper flex justify-between flex-grow">
+            <BtnPrimary
+              label="結帳"
+              @submit="CheckoutHandler"
+              theme="red"
+              px="px-3"
+            />
+            <BtnPrimary
+              theme="green"
+              class="whitespace-nowrap"
+              label="加入購物車"
+              @submit="AddToCartHandler"
+              px="px-2"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -113,6 +117,9 @@ export default {
 
 <style lang="scss">
 .card {
+  &:hover .wrapper {
+    @apply -translate-y-1 transition duration-200 shadow-2xl;
+  }
   .txtEllipsis {
     display: -webkit-box;
     -webkit-box-orient: vertical;
