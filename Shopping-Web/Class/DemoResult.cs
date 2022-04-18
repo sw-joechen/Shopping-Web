e77a6f85bf9bc0817d0dde28bda5a77b56b4494f
snackboy <swjoechen@gmail.com>
2022-04-18 14:24:39 +0800

    [test] DemoGetProductListByID 測試

---
+++ Shopping-Web/Class/DemoResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping_Web.Class {
+    public class DemoResult<T> {
+        public DemoResult(int code, string msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        public int code { get; set; }
+        public string msg { get; set; }
+        public T data { get; set; }
+
+        public void Set(int code, string msg, T data = default(T)) {
+            this.code = code;
+            this.msg = msg;
+            this.data = data;
+        }
+    }
+}
--- Shopping-Web/Controllers/ProductController.cs
+++ Shopping-Web/Controllers/ProductController.cs
@@ -9,10 +9,8 @@ using System.Web.Http;
 using Newtonsoft.Json;
 using Shopping_Web.Class;
 
-namespace Shopping_Web.Controllers
-{
-    public class ProductController : ApiController
-    {
+namespace Shopping_Web.Controllers {
+    public class ProductController : ApiController {
         string connectString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnDB"].ConnectionString;
 
         /// <summary>
@@ -20,14 +18,12 @@ namespace Shopping_Web.Controllers
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/getProductsList")]
-        public string GetProductsList()
-        {
+        public string GetProductsList() {
             var httpRequest = HttpContext.Current.Request;
             Result result = new Result(100, "缺少參數");
             List<Product> productList = new List<Product> { };
 
-            try
-            {
+            try {
                 string paramID = httpRequest.Params["id"];
                 string name = httpRequest.Params["name"] ?? null;
                 string type = httpRequest.Params["type"] ?? null;
@@ -41,11 +37,9 @@ namespace Shopping_Web.Controllers
                 Debug.WriteLine($"type=> {type}");
                 Debug.WriteLine($"enabled=> {enabled}");
 
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_saw_getProductList", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_saw_getProductList", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         if (id == -1)
                             cmd.Parameters.AddWithValue("@id", DBNull.Value);
@@ -71,12 +65,9 @@ namespace Shopping_Web.Controllers
 
                         SqlDataReader r = cmd.ExecuteReader();
 
-                        if (r.HasRows)
-                        {
-                            while (r.Read())
-                            {
-                                productList.Add(new Product
-                                {
+                        if (r.HasRows) {
+                            while (r.Read()) {
+                                productList.Add(new Product {
                                     id = Convert.ToInt32(r["f_id"]),
                                     name = r["f_name"].ToString(),
                                     description = r["f_description"].ToString(),
@@ -92,15 +83,13 @@ namespace Shopping_Web.Controllers
                             result.Set(200, "success", productList);
                             Debug.WriteLine($"productList=> {JsonConvert.SerializeObject(productList)}");
                         }
-                        else
-                        {
+                        else {
                             result.Set(112, "找不到該商品");
                         }
                     }
                 }
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 Debug.WriteLine($"ex: {ex}");
                 result.Set(101, "網路錯誤");
             }
@@ -112,50 +101,40 @@ namespace Shopping_Web.Controllers
         /// </summary>
         [HttpPost]
         [Route("api/{controller}/getProductListByID")]
-        public string GetProductListByID([FromBody] GetProductListByIDPayload payload)
-        {
+        public string GetProductListByID([FromBody] GetProductListByIDPayload payload) {
             Result result = new Result(100, "缺少參數");
             List<Product> productList = new List<Product> { };
 
             Debug.WriteLine($"payload=> {JsonConvert.SerializeObject(payload)}");
-            if (payload == null || payload.productIdList == null)
-            {
+            if (payload == null || payload.productIdList == null) {
                 return result.Stringify();
             }
 
             // 帶空陣列進來直接回空陣列結束
-            if (payload.productIdList.Count == 0)
-            {
+            if (payload.productIdList.Count == 0) {
                 result.Set(200, "success", productList);
             }
 
-            try
-            {
+            try {
                 List<int> productIDList = payload.productIdList;
 
                 // List轉成datatable
                 DataTable productDt = new DataTable();
                 productDt.Columns.Add("ID", typeof(int));
-                for (int i = 0; i < productIDList.Count; i++)
-                {
+                for (int i = 0; i < productIDList.Count; i++) {
                     productDt.Rows.Add(productIDList[i]);
                 }
 
-                using (SqlConnection conn = new SqlConnection(connectString))
-                {
+                using (SqlConnection conn = new SqlConnection(connectString)) {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("pro_sw_getProductListByID", conn))
-                    {
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_getProductListByID", conn)) {
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@ids", productDt);
                         SqlDataReader r = cmd.ExecuteReader();
 
-                        if (r.HasRows)
-                        {
-                            while (r.Read())
-                            {
-                                productList.Add(new Product
-                                {
+                        if (r.HasRows) {
+                            while (r.Read()) {
+                                productList.Add(new Product {
                                     id = Convert.ToInt32(r["f_id"]),
                                     name = r["f_name"].ToString(),
                                     description = r["f_description"].ToString(),
@@ -174,12 +153,76 @@ namespace Shopping_Web.Controllers
                     }
                 }
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 Debug.WriteLine($"ex: {ex}");
                 result.Set(101, "網路錯誤");
             }
             return result.Stringify();
+        }
+
+        /// <summary>
+        /// demo
+        /// </summary>
+        [HttpPost]
+        [Route("api/{controller}/DemoGetProductListByID")]
+        public DemoResult<List<Product>> DemoGetProductListByID([FromBody] GetProductListByIDPayload payload) {
+            var result = new DemoResult<List<Product>>(100, "缺少參數");
+            List<Product> productList = new List<Product> { };
+
+            Debug.WriteLine($"payload=> {JsonConvert.SerializeObject(payload)}");
+            if (payload == null || payload.productIdList == null) {
+                return result;
+            }
+
+            // 帶空陣列進來直接回空陣列結束
+            if (payload.productIdList.Count == 0) {
+                result.Set(200, "success", productList);
+                return result;
+            }
+
+            try {
+                List<int> productIDList = payload.productIdList;
+
+                // List轉成datatable
+                DataTable productDt = new DataTable();
+                productDt.Columns.Add("ID", typeof(int));
+                for (int i = 0; i < productIDList.Count; i++) {
+                    productDt.Rows.Add(productIDList[i]);
+                }
+
+                using (SqlConnection conn = new SqlConnection(connectString)) {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_getProductListByID", conn)) {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@ids", productDt);
+                        SqlDataReader r = cmd.ExecuteReader();
+
+                        if (r.HasRows) {
+                            while (r.Read()) {
+                                productList.Add(new Product {
+                                    id = Convert.ToInt32(r["f_id"]),
+                                    name = r["f_name"].ToString(),
+                                    description = r["f_description"].ToString(),
+                                    price = Convert.ToInt32(r["f_price"]),
+                                    picture = $"/Uploads{r["f_picture"]}",
+                                    amount = Convert.ToInt32(r["f_amount"]),
+                                    type = r["f_type"].ToString(),
+                                    enabled = Convert.ToBoolean(Convert.ToInt16(r["f_enabled"])),
+                                    createdDate = r["f_createdDate"].ToString(),
+                                    updatedDate = r["f_updatedDate"].ToString(),
+                                });
+                            }
+                            result.Set(200, "success", productList);
+                            Debug.WriteLine($"productList=> {JsonConvert.SerializeObject(productList)}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Debug.WriteLine($"ex: {ex}");
+                result.Set(101, "網路錯誤");
+            }
+            return result;
         }
     }
 }
